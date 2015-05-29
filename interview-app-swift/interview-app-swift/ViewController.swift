54c0cbc36a0eeedc7cb9c7a468643c7738d22f46
Tony Nuzzi <nuzzis@sbcglobal.net>
2015-05-28 19:17:02 -0500

    Swift project template

---
+++ interview-app-swift/interview-app-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  interview-app-swift
+//
+//  Created by Tony Nuzzi on 5/17/15.
+//  Copyright (c) 2015 Tony Nuzzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
