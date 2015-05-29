85c04ec99dbff18fc50dee53338b90cb5c029923
Tony Nuzzi <nuzzis@sbcglobal.net>
2015-05-28 19:19:39 -0500

    Updated AppDelegate to hold global CoffeeshopFinderServer

--- interview-app-objc/interview-app-objc/AppDelegate.h
+++ interview-app-objc/interview-app-objc/AppDelegate.h
@@ -7,11 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "CoffeeShopFinderService.h"
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
+@property (strong, nonatomic) CoffeeShopFinderService* coffeeShopFinderService;
 
 @end
 
