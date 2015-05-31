c018aaa575d9c81772252bce2d48d5141b732e99
Tony Nuzzi <nuzzis@sbcglobal.net>
2015-05-31 10:03:48 -0500

    Subclass PointAnnotation to add index for coffee shop lookup

---
+++ interview-app-objc/interview-app-objc/CoffeeShopPointAnnotation.h
@@ -0,0 +1,16 @@
+//
+//  CoffeeShopPointAnnotation.h
+//  interview-app-objc
+//
+//  Created by Tony Nuzzi on 5/28/15.
+//  Copyright (c) 2015 Tony Nuzzi. All rights reserved.
+//
+
+#import <MapKit/MapKit.h>
+#import "CoffeeshopModel.h"
+
+@interface CoffeeShopPointAnnotation : MKPointAnnotation
+
+@property (nonatomic) NSInteger index;
+
+@end
---
+++ interview-app-objc/interview-app-objc/CoffeeShopPointAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  CoffeeShopPointAnnotation.m
+//  interview-app-objc
+//
+//  Created by Tony Nuzzi on 5/28/15.
+//  Copyright (c) 2015 Tony Nuzzi. All rights reserved.
+//
+
+#import "CoffeeShopPointAnnotation.h"
+
+@implementation CoffeeShopPointAnnotation
+
+@end
