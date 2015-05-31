45ccc10a5cb391a2e931dc1cab5e353026b4e087
Tony Nuzzi <nuzzis@sbcglobal.net>
2015-05-31 10:01:39 -0500

    Convert to using UIAlertController rather then UIAlertView

--- interview-app-objc/interview-app-objc/YelpAPIService.m
+++ interview-app-objc/interview-app-objc/YelpAPIService.m
@@ -34,8 +34,6 @@
                  location,
                  latitude, longitude,
                  SEARCH_RESULT_LIMIT] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-
-//    NSLog(@"YELP URL: %@", urlString);
     
     // Initialize URL object
     NSURL *URL = [NSURL URLWithString:urlString];
@@ -55,7 +53,6 @@
     
     [request prepare];
     
-#warning Update to use NSURLSession
     // Initialize URLConnection
     NSURLConnection *conn = [[NSURLConnection alloc] initWithRequest:request delegate:self];
     
@@ -83,11 +80,15 @@
     // Log error to console
     NSLog(@"%@", errorString);
     
-    [[[UIAlertView alloc] initWithTitle:@"Error"
-                                message:@"Failed to connect to location server"
-                               delegate:nil
-                      cancelButtonTitle:@"OK"
-                      otherButtonTitles:nil] show];
+    // Initialize alert controller
+    UIAlertController* alertController = [UIAlertController alertControllerWithTitle:@"Error" message:@"Failed to connect to Yelp server." preferredStyle:UIAlertControllerStyleAlert];
+    
+    // Ok Action button
+    UIAlertAction* okAction = [UIAlertAction actionWithTitle:@"Ok" style:UIAlertActionStyleDefault handler:nil];
+    [alertController addAction:okAction];
+    
+    // Show Alert controller
+    [[[[[UIApplication sharedApplication] delegate] window] rootViewController] presentViewController:alertController animated:YES completion:nil];
 }
 
 - (void)connectionDidFinishLoading:(NSURLConnection *)connection {
@@ -104,6 +105,24 @@
         self.resultArray = [[NSMutableArray alloc] init];
     }
     
+    // Iterate through the JSON results and create CoffeeshopModel from each business
+    /* Consdensed version
+     {
+         "rating": 5.0,
+         "mobile_url": "http://m.yelp.com/biz/the-flying-falafel-san-francisco-3",
+         "rating_img_url": "http://s3-media1.fl.yelpcdn.com/assets/2/www/img/f1def11e4e79/ico/stars/v1/stars_5.png",
+         "review_count": 183,
+         "name": "The Flying Falafel",
+         "image_url": "http://s3-media1.fl.yelpcdn.com/bphoto/0f3f_XWaIfnx_0Lqq_vEeA/ms.jpg",
+         "display_phone": "+1-415-964-1003",
+         "location": {
+             "display_address": [
+             "1051 Market St",
+             "SoMa",
+             "San Francisco, CA 94103"
+             ]
+         }
+    */
     if (resultResponseDict && [resultResponseDict count] > 0) {
         if ([resultResponseDict objectForKey:@"businesses"] &&
             [[resultResponseDict objectForKey:@"businesses"] count] > 0) {
