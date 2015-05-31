7492ea51a12c2aa112aa6fb78878af09a06dfa03
Tony Nuzzi <nuzzis@sbcglobal.net>
2015-05-31 10:07:02 -0500

    Checkin oauth constants class

---
+++ interview-app-objc/interview-app-objc/OAuthAPIConstants.h
@@ -0,0 +1,18 @@
+//
+//  YelpAPIService.m
+//
+//  Adapted by Tony Nuzzi on 7/25/15
+//  Originally Created by Max Campolo on 7/16/14.
+//  https://github.com/maxcampolo/WeHungry
+
+
+#ifndef OAuthAPIConstants_h
+#define OAuthAPIConstants_h
+
+// DEFINE OAUTH CONSTANTS HERE
+#define OAUTH_CONSUMER_KEY @"Consumer Key"
+#define OAUTH_CONSUMER_SECRET @"Consumer Secret"
+#define OAUTH_TOKEN @"Token"
+#define OAUTH_TOKEN_SECRET @"Token Secret"
+
+#endif
